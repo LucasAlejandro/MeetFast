00a80b2dfb660af79701ecb6110eac66ccdf3814
LucasAlejandro <alejandrolucas9546@gmail.com>
2018-12-10 21:31:13 +0100

    script conexion con bbdd y tabla EVentos

--- testlogin/Controllers/ConexionBD.cs
+++ testlogin/Controllers/ConexionBD.cs
@@ -7,7 +7,7 @@ namespace MeetFastGit.Controllers
         /// <summary>
         /// Establece conexión con la base de datos
         /// </summary>
-        MySqlConnection conectar = new MySqlConnection("server=db4free.net; database=meetfast; Uid=meetfast_admin; pwd=Contraseña1;");
+        MySqlConnection conectar = new MySqlConnection("Server=tcp:meetfast.database.windows.net,1433;Initial Catalog=meetfast;Persist Security Info=False;User ID={meetfast_admin@meetfast};Password={Carlos3proyecto};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
         public MySqlConnection ObtenerConexion()
         {
             conectar.Open();
---
+++ testlogin/Controllers/GeoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using testlogin.datos;
+
+namespace mapsAPI.Controllers
+{
+    public class GeoController : Controller
+    {
+        meetfastEntitiesEvento context = new meetfastEntitiesEvento();
+        // GET: Geo
+        public ActionResult InfoWindow()
+        {
+            return View();
+        }
+        public ActionResult InfoWindow_bbdd()
+        {
+            return View();
+        }
+        public ActionResult Autocomplete()
+        {
+            return View();
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GeoCoder()
+        {
+            return View();
+        }
+        public ActionResult MarkerCluster()
+        {
+            return View();
+        }
+
+        public JsonResult GetAllLocation() {
+            var data = context.Evento.ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
